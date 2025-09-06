966e3837b8a246d0de1ac51fb62dbedf3a9387ae
Shreya Sarangi <shreyasarangi2006@gmail.com>
2025-09-06 23:44:14 +0530

    Update Array_search.java

--- Arrays/Array_search.java
+++ Arrays/Array_search.java
@@ -14,17 +14,17 @@ class Array_search {
     }
   }
   public static void main(String s[]) {
-		int[] arr = new int[50];
+	int[] arr = new int[50];
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter size of array: ");
     int arraySize = sc.nextInt();
-		System.out.print("Enter elements of array: ");
+	System.out.print("Enter elements of array: ");
 		for(int i = 0; i < arraySize; i++)
 		{	
 			arr[i] = sc.nextInt();
 		}
-		System.out.print("Enter element to search: ");
-		int x = sc.nextInt();
+	System.out.print("Enter element to search: ");
+	int x = sc.nextInt();
     convert(arr,arraySize,x);   
 	}
 }
