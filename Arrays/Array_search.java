d1a6a03b4a4017061b5c1e27055ff13b2ccc2cb6
Shreya Sarangi <shreyasarangi2006@gmail.com>
2025-09-06 23:43:07 +0530

    Create Array_search.java

---
+++ Arrays/Array_search.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Array_search {
+	
+	public static void convert(int arr[],int arraySize,int x)
+	{
+    int i;
+		for (i = 0; i < arraySize; i++)
+ 		{
+      if(arr[i] == x)
+			{
+			  System.out.println("Element is present at index "+i);
+			}
+    }
+  }
+  public static void main(String s[]) {
+		int[] arr = new int[50];
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter size of array: ");
+    int arraySize = sc.nextInt();
+		System.out.print("Enter elements of array: ");
+		for(int i = 0; i < arraySize; i++)
+		{	
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("Enter element to search: ");
+		int x = sc.nextInt();
+    convert(arr,arraySize,x);   
+	}
+}
+
